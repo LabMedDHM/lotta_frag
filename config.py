c95549196926bd98e627f2b8d15a09ad02b19572
Lotta von Braun <lottavvraun@t-online.de>
2026-01-12 16:43:23 +0000

    After Usage od specific mode, but still problems with filtering

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 BIN_SIZE = 100000  
-ANALYSIS_MODE = "all_vs_healthy"
-ANALYSIS_MODE = "specific_vs_healthy"
-SPECIFIC_GROUP = "colorectal"
+# ANALYSIS_MODE can be "all_vs_healthy" or "specific_vs_healthy"
+ANALYSIS_MODE = "specific_vs_healthy" 
+SPECIFIC_GROUP = "Colorectal Cancer" 
+# Bin sizes to test: 100000, 50000, 10000, 5000
