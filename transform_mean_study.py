d38d9d6d59509d7fcd45564420168f61689bd468
Lotta von Braun <lottavvraun@t-online.de>
2026-02-23 17:01:53 +0000

    23.02.26

--- transform_mean_study.py
+++ transform_mean_study.py
@@ -7,34 +7,57 @@ def main():
     ap.add_argument("--in_tsv", required=True, help="Input GC-corrected long TSV")
     ap.add_argument("--out_tsv", required=True, help="Output long TSV with transformed values corrected by study mean")
     ap.add_argument("--meta_tsv", required=True, help="Matrix storing study information")
+    ap.add_argument("--reference_csv", required=False, help="Loading csv with study means")
+    ap.add_argument("--save_reference_csv", required=False, help="Saving csv with study means")
     args = ap.parse_args()
-    # Liste der Metriken, die wir korrigieren wollen
+
+
     metrics = ["mean", "median", "stdev", "min", "max", "wps_value"]
+    study_references_loaded = None
+    if args.reference_csv:
+        study_references_loaded = pd.read_csv(args.reference_csv, index_col=0)
+        print("Loaded study references from:", args.reference_csv)
+        print(study_references_loaded)
     
     df = pd.read_csv(args.in_tsv, sep="\t")
-    corrected_df = df.copy()
 
-    meta = pd.read_csv(args.meta_tsv, sep=";")
-    meta_df = meta.copy()
-
-    df_combined = pd.merge (
-        df,
-        meta_df[["Extracted_ID", "Study"]],
-        left_on="sample",
-        right_on="Extracted_ID",
-        how="left"
-    )
-
-    # 1. Berechne die Healthy-Referenz pro Studie
-    # Wir filtern nur die 'Healthy' Proben und gruppieren nach der Spalte 'Study'
-    study_references = df_combined[df_combined["group"].str.lower() == "healthy"].groupby("Study")[metrics].mean()
     
-    print("Gefundene Healthy-Referenzen pro Studie:")
-    print(study_references)
+    if "Study" in df.columns:
+        print("'Study'-column already in Input – no merge needed.")
+        df_combined = df.copy()
+    else:
+        meta = pd.read_csv(args.meta_tsv, sep=";")
+        df_combined = pd.merge(
+            df,
+            meta[["Extracted_ID", "Study"]],
+            left_on="sample",
+            right_on="Extracted_ID",
+            how="left"
+        )
+
+    before = df_combined['sample'].nunique()
+    df_combined = df_combined[df_combined['Study'] != 'Sun']
+    after = df_combined['sample'].nunique()
+
+    print(f"Removed Sun samples: {before - after}")
+    print("Studies left:", df_combined['Study'].unique())
+
+    print("Spalten in df_combined:", df_combined.columns.tolist())
+    print(df_combined[["sample", "Study", "group"]].drop_duplicates("sample").head())
+
+    if study_references_loaded is not None:
+        study_references = study_references_loaded
+    else:
+        study_references = df_combined[df_combined["group"].str.lower() == "healthy"].groupby("Study")[metrics].mean()
+        if args.save_reference_csv:
+            study_references.to_csv(args.save_reference_csv)
+            print("Saved study references to:", args.save_reference_csv)
+        print("Calculated Healthy references per study:")
+        print(study_references)
     corrected_df = df_combined.copy()
 
     # 2. Korrektur anwenden
-    for study in corrected_df["Study"].unique():
+    for study in corrected_df["Study"].unique():    
             if pd.isna(study): continue # Falls eine Probe keine Study-Zuordnung hat
             
             if study in study_references.index:
@@ -52,6 +75,9 @@ def main():
     corrected_df.to_csv(args.out_tsv, sep="\t", index=False)
     print("Wrote:", args.out_tsv)
     print("Shape:", corrected_df.shape)
+
+    print(corrected_df[
+    corrected_df["group"].str.lower() == "healthy"].groupby("Study")[metrics].mean())
 
 
 if __name__ == "__main__":
